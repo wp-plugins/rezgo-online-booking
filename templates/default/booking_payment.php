f8155d62a80a1c4d2904a4e8bb19a7dbf6639645
rezgo <rezgo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-18 00:45:14 +0000

    Tagging version 1.6
    git-svn-id: https://plugins.svn.wordpress.org/rezgo-online-booking/tags/1.6@600302 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/default/booking_payment.php
@@ -0,0 +1,84 @@
+<?
+	if($_REQUEST['rezgoAction'] == 'return') {
+		echo '<script>parent.creditConfirm("'.$_REQUEST['token'].'");</script>';
+	}
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+<!--[if lte IE 6]><script src="<?=$this->path?>/javascript/ie6/warning.js"></script><script>window.onload=function(){e("<?=$this->path?>/javascript/ie6/")}</script><![endif]-->
+
+<link media="all" href="<?=$this->path?>/header.css" type="text/css" rel="stylesheet">
+<!--[if lte IE 7]>
+<link rel="stylesheet" type="text/css" href="<?=$this->path?>/header_ie.css" />
+<![endif]-->
+
+<script type="text/javascript" src="<?=$this->path?>/javascript/jquery.tools.min.js"></script>
+
+<? if($site->exists($site->getStyles())) { ?>
+<style>
+<!--
+
+	<?=$site->getStyles()?>
+
+-->
+</style>
+<? } ?>
+
+</head>
+<body style="margin:0px; padding:0px; background:#ffffff;">
+
+	<form method="post" id="payment" action="https://process.rezgo.com/form">
+	
+	<input type="hidden" name="return" value="https://<?=$_SERVER['HTTP_HOST'].REZGO_URL_BASE?>/booking_payment.php?rezgoAction=return&">
+	
+	<div id="rezgo" style="width:310px; margin:0px; padding:0px; border-radius:0px; background:#FFF;">
+	
+	<div class="payment_info" id="payment_info" style="margin:0px; width:100%; padding:5px;">
+					
+		<div id="payment_cards">
+			<ol>
+				
+				<li><label class="left" style="width:45%;">Cardholder Name<em></em></label><input type="text" id="name" name="name" value="" /></li>
+				<li><label class="left" style="width:45%;">Card Number<em></em></label><input type="text" id="pan" name="pan" value="" /></li>
+				<li>
+					<label class="left" style="width:45%;">Card Expiry<em></em></label>
+					<select name="exp_month" id="exp_month" style="position:relative; top:-5px;">
+						<option value="01">01</option>
+						<option value="02">02</option>
+						<option value="03">03</option>
+						<option value="04">04</option>
+						<option value="05">05</option>
+						<option value="06">06</option>
+						<option value="07">07</option>
+						<option value="08">08</option>
+						<option value="09">09</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+						<option value="12">12</option>
+					</select>
+					<select name="exp_year" id="exp_year" style="position:relative; top:-5px;">
+						<? for($d=date("Y"); $d <= date("Y")+12; $d++): ?>
+							<option value="<?=substr($d, -2)?>"><?=$d?></option>
+						<? endfor; ?>
+					</select>	
+				</li>
+		
+				<? if($site->getCVV()) { ?>
+				<li><label class="left" style="width:45%;">CVV Number<em></em></label><input type="text" name="cvv" id="cvv" value="" style="width:50px;" />&nbsp;<a href="javascript:void(0);" onclick="javascript:window.open('<?=$site->path?>/images/cv_card.jpg',null,'width=600,height=300,status=no,toolbar=no,menubar=no,location=no');">what is this ?</a></li>
+				<? } ?>
+			</ol>
+			
+		</div>
+			
+		</div>
+	
+	</div>
+	
+	</form>
+
+</body>
+</html>
